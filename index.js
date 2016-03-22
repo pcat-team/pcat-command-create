d1e1211de1e0256a53cca2814c3a124da05bdeeb
Jin_C <275727449@qq.com>
2016-03-22 11:46:13 +0800

    change temp
    change temp

--- index.js
+++ index.js
@@ -2,52 +2,87 @@
 const path = require('path')
 const fse  = require('fs-extra')
 const fs   = require('fs')
+const projectPath = fis.project.getProjectPath()
+const siteReg = /\/(pc(?:online|auto|lady|house|baby|games))\//i
+const site = siteReg.test(projectPath) ? RegExp.$1 : ''
+
+if(!site)return fis.log.info('Can\'t find site dirname'.red.bold);
+var __path = projectPath.split(`/${site}/`)
+const dir  = __path[1]
 
 var tempFn = function(fn){
   return fn.toString().replace(/.*?\/\*(.*?)\*\//gmi,'$1')
 }
 var packContent = {
-      "name": "{{temp}}",
-      "version": "0.0.1",
-      "private": true,
-      "main": "page/index/index.html"
-    }
+  "name": "{{temp}}",
+  "version": "0.0.1",
+  "dir":dir,
+  "site": site
+}
 
 exports.name = 'create <command> [options]'
 // exports.usage = '<commad> [option]'
-exports.desc = 'create dir or widget for deferant works'
+exports.desc = 'create dir , widget or page for deferant works'
 exports.options = {
-  '-w,--widget <widget name> ':'create widget'
+  '-w,--widget <widget name> ':'create widget',
+  '-p,--page <page name> ':'create static page',
+  '-c,--cms <cms page name>':'create cms page'
 };
 exports.run = function(argv, cli, env) {
-  
+  'use strict'
   if ((argv.h === true) || argv.help) {
     return cli.help(exports.name, exports.options);
   }
   var command = argv._
   var _name = argv.w || argv.widget
   var _page = argv.p || argv.page
-  var dirs = ['page','widget']
-  var files = ['fis-conf.js','map.json','package.json']
+  var _cms  = argv.c || argv.cms
 
   if(_name){
-    let dir = fis.project.getProjectPath() + '/'
+    let dir = projectPath + '/'
+    let wDir = path.resolve(dir,'./widget/'+_name)
+    if(fs.existsSync(wDir))return fis.log.warn(`${_name} is exists\n` ,wDir.red.bold);
     ;[_name+'.html',_name+'.css',_name+'.js'].forEach(function(name){
+      fse.outputFile(path.resolve(dir,'./widget/'+_name + '/'+ name),'')
       fis.log.info(path.resolve(dir,'./widget/'+_name + '/'+ name).yellow.bold  + ' is created success!')
     })
   }else if(_page){
-    let dir = fis.project.getProjectPath() + '/'
-    ;[_name+'.html',_name+'.css',_name+'.js',_name + '_cms.json'].forEach(function(name){
-      fse.outputFile(path.resolve(dir,'./page/'+_name + '/'+ name),'')
-      fis.log.info(path.resolve(dir,'./page/'+_name + '/'+ name).yellow.bold  + ' is created success!')
+    let dir = projectPath + '/'
+    let pDir = path.resolve(dir,'./page/'+_page)
+    if(fs.existsSync(pDir))return fis.log.warn(`${_page} is exists\n` ,wDir.red.bold);
+
+    ;['.html','.css','.js'].forEach(function(name){
+      let target = `./page/${_page}/${_page+name}`
+      let read   = `temp/page/dev/dev${name}`
+      
+      fse.readFile(path.resolve(__dirname,read),function(err,data){
+        err && fis.log.info(err.red.bold)
+        fse.outputFile(path.resolve(dir,target),data)
+        fis.log.info(path.resolve(dir,target).yellow.bold + ' is created success!')
+      })
+    })
+  }else if(_cms){
+    let dir = projectPath + '/'
+    let pDir = path.resolve(dir,'./page/'+_cms)
+    if(fs.existsSync(pDir))return fis.log.warn(`${_cms} is exists\n` ,wDir.red.bold);
+    ;['.html','.css','.js'].forEach(function(name){
+      let target = `./page/${_cms}/${_cms+name}`
+      let read   = `${(name === '.html' ? 'ctemp/page_cms' : 'temp/page/dev/dev')}${name}`
+      fse.readFile(path.resolve(__dirname,read),function(err,data){
+        err && fis.log.info(err.red.bold)
+        fse.outputFile(path.resolve(dir,target),data)
+        fis.log.info(path.resolve(dir,target).yellow.bold + ' is created success!')
+      })
     })
   }else if(command[1]){
     // mkdirp([])
-    let target = path.resolve(fis.project.getProjectPath(),command[1])
+    let _name = command[1]
+    let target = path.resolve(projectPath,_name)
+    if(fs.existsSync(target))return fis.log.warn(`${_name} is exists\n` ,target.red.bold);
     fse.copy(path.resolve(__dirname,'temp'),target, function (err) {
       if (err) return fis.log.error(err)
-      fis.log.info(command[1].yellow.bold  + ' is created success!')
-      packContent.name = command[1]
+      fis.log.info(_name.yellow.bold  + ' is created success!')
+      packContent.name = _name
       fse.outputJson(path.resolve(target,'package.json'),packContent,function(err){
         if(err) return fis.log.error(path.resolve(target,'package.json').red.bold )
       })
--- temp/fis-conf.js
+++ temp/fis-conf.js
@@ -6,8 +6,18 @@ fis.pcat({
   combo:!0,
   domain : {
       dev: '',
-      qa: '',
-      ol: ''
+      qa: {
+        'static': 'http://ue.pc.com.cn',
+        'img': 'http://ueimg.pc.com.cn',
+        'page':'http://pcat.pc.com.cn/qa/page',
+        'tpl':'http://pcat.pc.com.cn/qa/tpl'
+      },
+      ol: {
+        'static': 'http://ue.3conline.com',
+        'img': 'http://ueimg.3conline.com',
+        'page':'http://pcat.pc.com.cn/ol/page',
+        'tpl':'http://pcat.pc.com.cn/ol/tpl'
+      }
   },
   packageJson:packageJson
 })
