d198dcfdfe414c8da0abfc50d1286ba74349c322
Jin_C <275727449@qq.com>
2016-04-19 16:20:23 +0800

    1.1.13
    1.1.13

--- temp/fis-conf.js
+++ temp/fis-conf.js
@@ -5,19 +5,7 @@ var packageJson = require('./package.json');
 fis.pcat({
   combo:!0,
   domain : {
-      dev: '',
-      qa: {
-        'static': 'http://ue.pc.com.cn',
-        'img': 'http://ueimg.pc.com.cn',
-        'page':'http://pcat.pc.com.cn/qa/page',
-        'tpl':'http://pcat.pc.com.cn/qa/tpl'
-      },
-      ol: {
-        'static': 'http://ue.3conline.com',
-        'img': 'http://ueimg.3conline.com',
-        'page':'http://pcat.pc.com.cn/ol/page',
-        'tpl':'http://pcat.pc.com.cn/ol/tpl'
-      }
+      dev: ''
   },
   packageJson:packageJson
 })
--- temp/page/dev/dev.js
+++
@@ -1,1 +0,0 @@
- require.config(<!--REQUIRE_CONFIG-->)
