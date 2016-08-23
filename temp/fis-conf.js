a3417dd0999d0d8ababb11f92b1a38a013cae727
guoxuemin <guo469477762@gmail.com>
2016-08-23 16:44:12 +0800

    修改专题模板，ssi方式引入

--- temp/fis-conf.js
+++ temp/fis-conf.js
@@ -3,6 +3,9 @@ var packageJson = require('./package.json');
 
 fis.pcat({
   combo:!0,
+  ssiDomain:{
+  	// "/3g/pconline/xxxx/index.html":"http://g.pconline.com.cn"
+  },
   domain : {
       dev: ''
   },
