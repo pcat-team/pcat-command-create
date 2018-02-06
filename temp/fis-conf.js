4f1ae5602b988862e9c992a511008a9b56456990
guoxuemin <guo469477762@gmail.com>
2018-02-06 18:34:56 +0800

    修改config默认配置

--- temp/fis-conf.js
+++ temp/fis-conf.js
@@ -14,11 +14,13 @@ fis.pcat({
     },
     packageJson: packageJson,
     // 自动给 css 属性添加前缀，让标准的 css3 支持更多的浏览器.
+    // 详细配置参考:https://github.com/ai/browserslist#queries
     autoprefixerConfig: {
-        "browsers": ["Android >= 4.1", "iOS >= 9", "ie >= 8", "firefox >= 50"],
+        "browsers": ["Android >= 2.1", "iOS >= 4", "ie >= 8", "firefox >= 15"],
         "cascade": true
     },
     // es6、es7 或者 jsx 编译成 es5
     // 默认开启了 preset-2015 preset-stage-3 preset-react
-    babelConfig: {},
+    // 具体的 babel 配置参数可以参考 https://developit.github.io/babel-legacy-docs/
+    babelConfig: {}
 })
